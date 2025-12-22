cec34e95b08d0b1a7f55e611e8ae3b96b6b2c43e
Akila Pilapitiya <pilapit-im22056@stu.kln.ac.lk>
2025-12-22 17:08:59 +0530

    chore: Backend Basic Setup

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,22 @@
 import express from 'express';
-import {PORT} from './configs/env.js';
+import {PORT} from './src/configs/env.js';
+import authRouter from './src/routes/auth.routes.js'
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.use('/auth', authRouter);
 
+// Error handling Middleware
+
+// Server Running
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
