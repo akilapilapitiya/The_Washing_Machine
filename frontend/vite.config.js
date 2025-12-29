89d9a99c7ec72e4a1c909851e24965f04e066ab2
Akila Pilapitiya <pilapit-im22056@stu.kln.ac.lk>
2025-12-29 22:27:45 +0530

    chore: tailwind init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { PORT } from './src/configs/env'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   server: {
     port: Number(PORT) || 5173,
   },
