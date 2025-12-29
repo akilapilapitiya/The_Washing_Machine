6f413718d994a75b6ea04253d3c194f5efd03d7d
Akila Pilapitiya <pilapit-im22056@stu.kln.ac.lk>
2025-12-29 22:20:47 +0530

    chore: Enviroment Setup

---
+++ frontend/src/configs/env.js
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+
+const envFile = `.env.${process.env.NODE_ENV || "development"}.local`;
+config({ path: envFile });
+
+export const {
+  PORT
+} = process.env;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { PORT } from './src/configs/env'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(PORT) || 5173,
+  },
 })
