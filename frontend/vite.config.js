556c09284f70b5c922d6b3314275f76ee393d1fb
Akila Pilapitiya <pilapit-im22056@stu.kln.ac.lk>
2025-12-29 22:32:50 +0530

    chore:ShadCN config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { PORT } from './src/configs/env'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     port: Number(PORT) || 5173,
   },
