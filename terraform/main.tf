81064d31723cc9d5d97f18301c979173b8941626
Akila Pilapitiya <pilapit-im22056@stu.kln.ac.lk>
2026-03-09 09:50:12 +0530

    feat: HTTPS with app.truegate.live

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,6 +65,18 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "main" {
